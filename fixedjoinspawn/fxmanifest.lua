15dcd4ee0e70a3ea3009d3a76f669f895c3569c6
Pharma <34245836+logansportfolio@users.noreply.github.com>
2026-03-23 21:13:25 -0500

    Create fxmanifest.lua

---
+++ fixedjoinspawn/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Logan L.'
+description 'Force all players to spawn at one coordinate on every server join only'
+version '1.0.0'
+
+client_script 'client.lua'
